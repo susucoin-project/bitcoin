bca78b791104e1b2cb9d6b912c878e208849f045
Matt Corallo <git@bluematt.me>
2018-07-09 20:06:39 -0400

    Remove useless mapRequest tracking that just effects Qt display.
    I thought we had removed this a long time ago, TBH, its really
    confusing feedback to users that we display whether a tx was
    broadcast to immediate neighbor nodes, given that has little
    indication of whether the tx propagated very far.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -472,7 +472,6 @@ public:
     bool IsTrusted() const;
 
     int64_t GetTxTime() const;
-    int GetRequestCount() const;
 
     // RelayWalletTransaction may only be called if fBroadcastTransactions!
     bool RelayWalletTransaction(CConnman* connman);
@@ -806,7 +805,6 @@ public:
 
     int64_t nOrderPosNext = 0;
     uint64_t nAccountingEntryNumber = 0;
-    std::map<uint256, int> mapRequestCount;
 
     std::map<CTxDestination, CAddressBookData> mapAddressBook;
 
@@ -1048,16 +1046,6 @@ public:
     bool DelAddressBook(const CTxDestination& address);
 
     const std::string& GetLabelName(const CScript& scriptPubKey) const;
-
-    void Inventory(const uint256 &hash) override
-    {
-        {
-            LOCK(cs_wallet);
-            std::map<uint256, int>::iterator mi = mapRequestCount.find(hash);
-            if (mi != mapRequestCount.end())
-                (*mi).second++;
-        }
-    }
 
     void GetScriptForMining(std::shared_ptr<CReserveScript> &script);
 
